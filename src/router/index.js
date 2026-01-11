526cc58e84d88ee6b3ce7c6c0a8adc3f1dae45ac
egorpolushin <egscoots@mail.ru>
2026-01-11 16:15:47 +0300

    fal commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: () => import('../views/main/Index.vue'),
+    },
+    {
+      path: '/products',
+      name: 'products.index',
+      component: () => import('../views/products/Index.vue'),
+    },
+    {
+      path: '/products/:id',
+      name: 'products.show',
+      component: () => import('../views/products/Show.vue'),
+    },
+    {
+      path: '/cart',
+      name: 'cart.index',
+      component: () => import('../views/cart/Index.vue'),
+    },
+  ],
+})
+
+export default router
